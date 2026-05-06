38dfcaa27fb5957f78d82f7e6b3d0770da23bdb0
JavierAle <javieragustinale@gmail.com>
2026-05-06 18:53:56 +0200

    Working on filtering inpput logic

--- src/features/users/components/UserTable.tsx
+++ src/features/users/components/UserTable.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { getUsers } from '../services/userService.ts';
 import { IUser } from '../types/IUser.ts';
+import filterUsers from '../utils/filterUsers.ts';
 import '../styles/user-table.css';
 import LoadingSpinner from '../../../shared/features/loading-spinner/components/LoadingSpinner.tsx';
 
@@ -13,6 +14,8 @@ export default function UserTable({
     userSelected,
 }: UserTableProps) {
     const [users, setUsers] = useState<IUser[]>([]);
+    const [filteredUsers, setFilteredUsers] = useState<IUser[]>([]);
+    const [searchTerm, setSearchTerm] = useState<string>('');
     const [loading, setLoading] = useState<boolean>(false);
 
     useEffect(() => {
@@ -20,6 +23,7 @@ export default function UserTable({
         getUsers()
             .then((users: IUser[]) => {
                 setUsers(users);
+                setFilteredUsers(users);
             })
             .catch((err) => {
                 console.error(err);
@@ -27,50 +31,65 @@ export default function UserTable({
             .finally(() => setLoading(false));
     }, []);
 
+    
+    useEffect(() => {
+        const data = filterUsers(searchTerm, users);
+        setFilteredUsers(data);
+        console.log(data);
+    }, [searchTerm, users]);
+
     return (
-        <table className="users-table">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>User Name</th>
-                    <th>Phone</th>
-                </tr>
-            </thead>
-            <tbody>
-                {loading ? (
+        <>
+            <input
+                type="text"
+                placeholder="Filter by name"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <table className="users-table">
+                <thead>
                     <tr>
-                        <td
-                            colSpan={5}
-                            style={{
-                                textAlign: 'center',
-                                padding: '60px 15px',
-                            }}
-                        >
-                            <LoadingSpinner showLoading={loading} />
-                        </td>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>User Name</th>
+                        <th>Phone</th>
                     </tr>
-                ) : users.length ? (
-                    users.map((user) => (
-                        <tr
-                            key={user.id}
-                            onClick={() => handleShowPostsToggle(user)}
-                            className={
-                                userSelected?.id === user.id
-                                    ? 'user-selected'
-                                    : ''
-                            }
-                        >
-                            <td>{user.id}</td>
-                            <td>{user.name}</td>
-                            <td>{user.email}</td>
-                            <td>{user.username}</td>
-                            <td>{user.phone}</td>
+                </thead>
+                <tbody>
+                    {loading ? (
+                        <tr>
+                            <td
+                                colSpan={5}
+                                style={{
+                                    textAlign: 'center',
+                                    padding: '60px 15px',
+                                }}
+                            >
+                                <LoadingSpinner showLoading={loading} />
+                            </td>
                         </tr>
-                    ))
-                ) : null}
-            </tbody>
-        </table>
+                    ) : filteredUsers.length ? (
+                        filteredUsers.map((user) => (
+                            <tr
+                                key={user.id}
+                                onClick={() => handleShowPostsToggle(user)}
+                                className={
+                                    userSelected?.id === user.id
+                                        ? 'user-selected'
+                                        : ''
+                                }
+                            >
+                                <td>{user.id}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.username}</td>
+                                <td>{user.phone}</td>
+                            </tr>
+                        ))
+                    ) : null}
+                </tbody>
+            </table>
+        </>
     );
 }
---
+++ src/features/users/utils/filterUsers.ts
@@ -0,0 +1,11 @@
+import { IUser } from "../types/IUser";
+
+export default function filterUsers(searchTerm: string, data: IUser[]): IUser[] {
+    if(!searchTerm) return data;
+    const termLowerCase = searchTerm.toLocaleLowerCase();
+    return data.filter((item:IUser) => item.name.toLocaleLowerCase().includes(termLowerCase) ||
+    item.username.toLocaleLowerCase().includes(termLowerCase) ||
+    item.email.toLocaleLowerCase().includes(termLowerCase) ||
+    item.phone.toLocaleLowerCase().includes(termLowerCase)
+)
+}
