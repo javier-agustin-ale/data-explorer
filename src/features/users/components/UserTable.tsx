634cf349cf734de0b22a9029721ee16fb5845d47
JavierAle <javieragustinale@gmail.com>
2026-05-07 11:40:18 +0200

    Added logic for users-table filer input

--- src/features/users/components/UserTable.tsx
+++ src/features/users/components/UserTable.tsx
@@ -35,61 +35,116 @@ export default function UserTable({
     useEffect(() => {
         const data = filterUsers(searchTerm, users);
         setFilteredUsers(data);
-        console.log(data);
     }, [searchTerm, users]);
+
+    const isMatchingField = (fieldValue: string): boolean => {
+        if (searchTerm === '') return false;
+        return fieldValue
+            .toLocaleLowerCase()
+            .includes(searchTerm.toLocaleLowerCase());
+    };
 
     return (
         <>
-            <input
-                type="text"
-                placeholder="Filter by name"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-            />
-            <table className="users-table">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>User Name</th>
-                        <th>Phone</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {loading ? (
+            <div className="user-table-container">
+                <div className="search-container">
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        className="search-input"
+                        spellCheck={false}
+                    />
+                    {searchTerm && (
+                        <button
+                            className="search-button"
+                            onClick={() => setSearchTerm('')}
+                        >
+                            X
+                        </button>
+                    )}
+                </div>
+                <table className="users-table">
+                    <thead>
                         <tr>
-                            <td
-                                colSpan={5}
-                                style={{
-                                    textAlign: 'center',
-                                    padding: '60px 15px',
-                                }}
-                            >
-                                <LoadingSpinner showLoading={loading} />
-                            </td>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>User Name</th>
+                            <th>Phone</th>
                         </tr>
-                    ) : filteredUsers.length ? (
-                        filteredUsers.map((user) => (
-                            <tr
-                                key={user.id}
-                                onClick={() => handleShowPostsToggle(user)}
-                                className={
-                                    userSelected?.id === user.id
-                                        ? 'user-selected'
-                                        : ''
-                                }
-                            >
-                                <td>{user.id}</td>
-                                <td>{user.name}</td>
-                                <td>{user.email}</td>
-                                <td>{user.username}</td>
-                                <td>{user.phone}</td>
+                    </thead>
+                    <tbody>
+                        {loading ? (
+                            <tr>
+                                <td colSpan={5} className="no-results">
+                                    <LoadingSpinner showLoading={loading} />
+                                </td>
                             </tr>
-                        ))
-                    ) : null}
-                </tbody>
-            </table>
+                        ) : filteredUsers.length ? (
+                            filteredUsers.map((user) => (
+                                <tr
+                                    key={user.id}
+                                    onClick={() => handleShowPostsToggle(user)}
+                                    className={
+                                        userSelected?.id === user.id
+                                            ? 'user-selected'
+                                            : ''
+                                    }
+                                >
+                                    <td>{user.id}</td>
+                                    <td
+                                        className={
+                                            isMatchingField(user.name)
+                                                ? 'highlighted-text'
+                                                : ''
+                                        }
+                                    >
+                                        {user.name}
+                                    </td>
+                                    <td
+                                        className={
+                                            isMatchingField(user.email)
+                                                ? 'highlighted-text'
+                                                : ''
+                                        }
+                                    >
+                                        {user.email}
+                                    </td>
+                                    <td
+                                        className={
+                                            isMatchingField(user.username)
+                                                ? 'highlighted-text'
+                                                : ''
+                                        }
+                                    >
+                                        {user.username}
+                                    </td>
+                                    <td
+                                        className={
+                                            isMatchingField(user.phone)
+                                                ? 'highlighted-text'
+                                                : ''
+                                        }
+                                    >
+                                        {user.phone}
+                                    </td>
+                                </tr>
+                            ))
+                        ) : (
+                            <tr>
+                                <td
+                                    colSpan={5}
+                                    className="no-results highlighted-text"
+                                >
+                                    No users found
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 }
